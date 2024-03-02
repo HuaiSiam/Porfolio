65fe7b98ca5a22720034c4bf463848bb2514e323
huai siam <cinghuaiisiiam@gmail.com>
2024-03-03 00:08:33 +0630

    fix:typescript type

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,47 @@ import Profile from './components/Profile';
 import './App.css'
 import About from './components/About';
 import Experience from './components/Experience';
+import { useRef, useState, useEffect } from 'react';
 
 function App() {
+  const [itemactive, setitemactive] = useState('about');
+  const sectionRefs = useRef([] as any);
+
+  useEffect(() => {
+      const handleScroll = () => {
+        const scrollPosition = window.scrollY;
+  
+        // Find the active section based on scroll position
+        const newActiveSection = sectionRefs.current.find((item:any) => {
+          return scrollPosition <= item.offsetTop
+        });
+
+        setitemactive(newActiveSection.id)
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      // Clean up event listener on component unmount
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+  }, []);
+
+  const handleCallback = (childData:any) => setitemactive(childData);
 
   return (
     <div className='main-container grid grid-cols-1 lg:grid-cols-2 xl:gap-5'>
       <div className="lg:py-24 flex flex-col justify-between lg:sticky lg:max-h-screen lg:top-0">
-        <Profile />
+        <Profile isActive={itemactive} parentCallback={handleCallback} />
       </div>
 
       <div className='pt-24 lg:py-24 main-content'>
-        <section id='about'
+        <section id='about' ref={ref => ref?.offsetTop!=undefined ? (sectionRefs.current[0] = { id: 'about', ref, offsetTop: ref.offsetTop }) : null}
         className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24'>
           <About />
         </section>
 
-        <section id='experience'
+        <section id='experience' ref={ref => ref?.offsetTop!=undefined ? (sectionRefs.current[1] = { id: 'experience', ref, offsetTop: ref.offsetTop }) : null}
         className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24'>
           <Experience />
         </section>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
-function Banner(){
-    const [isActive, setisActive] = useState('about');
-
+const Profile = (props:any) =>{
     return(
         <>
             <div>
@@ -14,13 +10,13 @@ function Banner(){
 
                 <ul className="mt-16 w-max profile-sidebar">
                     <li>
-                        <a className={`group flex items-center py-3 sidebar-item ${isActive == 'about' && 'active'}`} href="#about" onClick={() => setisActive('about')}>
+                        <a className={`group flex items-center py-3 sidebar-item ${props.isActive == 'about' && 'active'}`} href="#about" onClick={() => props.parentCallback('about')}>
                             <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
                             <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">About</span>
                         </a>
                     </li>
                     <li>
-                        <a className={`group flex items-center py-3 sidebar-item ${isActive == 'experience' && 'active'}`} href="#experience" onClick={() => setisActive('experience')}>
+                        <a className={`group flex items-center py-3 sidebar-item ${props.isActive == 'experience' && 'active'}`} href="#experience" onClick={() => props.parentCallback('experience')}>
                             <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
                             <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">experience</span>
                         </a>
@@ -43,4 +39,4 @@ function Banner(){
     )
 }
 
-export default Banner;
+export default Profile;
